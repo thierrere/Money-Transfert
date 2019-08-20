47d1eb076945b113d256c9acf61d1bc6e9a3de1a
Nenkam Nenkam, T <t.nenkam.nenkam@accenture.com>
2019-08-20 10:09:25 +0400

    Writing the test for AccountDao

--- src/main/kotlin/transfer/com/dao/AccountDaoImpl.kt
+++ src/main/kotlin/transfer/com/dao/AccountDaoImpl.kt
@@ -3,6 +3,30 @@ package transfer.com.dao
 import transfer.com.model.Account
 
 object AccountDaoImpl : AccountDao {
+
+    /**
+     * In-Memory datastore
+     */
+    private var accounts = hashMapOf(
+        "account01@mail.com" to Account (email = "account01@mail.com", balance  = 1000.0),
+        "account02@mail.com" to Account (email = "account02@mail.com", balance  = 2000.0),
+        "account03@amail.com" to Account (email = "account03@amail.com", balance  = 3000.0),
+        "account04@mail.com" to Account (email = "account04@mail.com", balance  = 4000.0)
+    )
+
+    /**
+     * Method to return all the accounts
+     */
+    fun getAccounts() : HashMap<String, Account>{
+        return accounts
+    }
+
+    fun setAccounts(accountToSet : HashMap<String, Account>) {
+        if (accountToSet.size > 0){
+            accounts=accountToSet
+        }
+    }
+
     override fun findByEmail(email: String): Account? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
---
+++ src/test/kotlin/transfer/com/dao/AccountDaoImplTest.kt
@@ -0,0 +1,65 @@
+package transfer.com.dao
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import transfer.com.model.Account
+import kotlin.test.BeforeTest
+import kotlin.test.assertNotNull
+
+class AccountDaoImplTest {
+
+    private val accountDao : AccountDaoImpl = AccountDaoImpl
+    @BeforeTest
+    fun prepareTest (){
+        val accounts = hashMapOf(
+            "test01@mail.com" to Account (email = "test01@mail.com", balance  = 1000.0),
+            "test02@mail.com" to Account (email = "test02@mail.com", balance  = 2000.0),
+            "test03@mail.com" to Account (email = "test03@mail.com", balance  = 3000.0),
+            "test04@mail.com" to Account (email = "test04@mail.com", balance  = 4000.0)
+        )
+        accountDao.setAccounts (accountToSet = accounts)
+    }
+
+    @Test
+    fun findByEmailTest(){
+        runBlocking {
+            assertNotNull(accountDao.findByEmail("test01@mail.com"))
+            assertThat(accountDao.getAccounts()["test01@mail.com"]).isEqualTo(accountDao.findByEmail(accountDao.getAccounts()["test01@mail.com"]!!.getEmail()))
+        }
+    }
+
+    @Test
+    fun depositTest() {
+        runBlocking {
+            val account = accountDao.getAccounts()["test01@mail.com"]
+            assertNotNull(accountDao.findByEmail(account!!.getEmail()))
+            val depositValue = 200.0
+            val afterDepositValue = depositValue+account.getBalance()
+            accountDao.deposit(account.getEmail(), depositValue)
+            assertThat(Account(email = account.getEmail(), balance = afterDepositValue)).isEqualTo(accountDao.getAccounts()["test01@mail.com"])
+        }
+    }
+
+    @Test
+    fun withdrawTest () {
+        runBlocking {
+            val account = accountDao.getAccounts()["test02@mail.com"]
+            assertNotNull(accountDao.findByEmail(account!!.getEmail()))
+            val withdrawValue = 200.0
+            val afterWithdrawValue = account.getBalance() - withdrawValue
+            accountDao.withdraw(account.getEmail(), withdrawValue)
+            assertThat(Account(email = account.getEmail(), balance = afterWithdrawValue)).isEqualTo(accountDao.getAccounts()["test02@mail.com"])
+        }
+    }
+
+    @Test
+    fun balanceTest () {
+        runBlocking {
+            val account = accountDao.getAccounts()["test03@mail.com"]
+            assertNotNull(accountDao.findByEmail(account!!.getEmail()))
+            assertThat(account).isEqualTo(Account(email = account.getEmail(), balance = accountDao.balance(account.getEmail())))
+        }
+    }
+
+}
