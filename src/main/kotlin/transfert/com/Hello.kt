272011d47e4a9a1fbe781ad2e98c46f1673ca5b6
Nenkam Nenkam, T <t.nenkam.nenkam@accenture.com>
2019-08-20 09:39:34 +0400

    Initializing the project

---
+++ src/main/kotlin/transfert/com/Hello.kt
@@ -0,0 +1,6 @@
+package transfert.com
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/transfert/com/HelloTest.kt
@@ -0,0 +1,8 @@
+package transfert.com
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
